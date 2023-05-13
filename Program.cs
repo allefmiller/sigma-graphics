ee18482b3247b85c31477965dbef7a7f5c01963a
Allef Miller <allefmiller@hotmail.com>
2023-05-13 20:55:39 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace sigma_graphcs
+{
+    internal class Program : GameWindow
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+    }
+}
