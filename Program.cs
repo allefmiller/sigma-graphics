dd50ca6075a1b695a93d2f165d8f2e8b878bbc2f
Allef Miller <allefmiller@hotmail.com>
2023-05-13 22:44:45 -0300

    example of how to create rectangle object

--- Program.cs
+++ Program.cs
@@ -5,15 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
 namespace sigma_graphcs
 {
-    internal class Program : GameWindow
+    class Program : GameWindow
     {
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            //GL.LoadMatrix(ref projection);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Color3(1.0f, 1.0f, 0.0f);
+            GL.Begin(PrimitiveType.Quads);
+            GL.Vertex2(-0.1f, -0.1f);
+            GL.Vertex2(0.1f, -0.1f);
+            GL.Vertex2(0.1f, 0.1f);
+            GL.Vertex2(-0.1f, 0.1f);
+            GL.End();
+
+            SwapBuffers();
+        }
         static void Main(string[] args)
         {
             new Program().Run();
         }
     }
+
 }
